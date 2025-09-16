c5819883ec5a231b1c8fe7d975bf858f77f627d3
Giovanni Favaro <wip.gio@gmail.com>
2025-09-16 15:27:27 +0200

    No code changes detected.

--- test_discover.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-from nav_builder import discover_markdown_files
-
-content_root = Path('unstablecode/content')
-md_files = discover_markdown_files(content_root)
-print(f'Discovered {len(md_files)} markdown files:')
-for mf in md_files:
-    print(f'  {mf.relative_to(content_root)}')
